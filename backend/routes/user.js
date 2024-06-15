d3d5d4bbba2b97c3abf8eade08605f77d121b369
Ashish <ashishktiwari1203@gmail.com>
2024-06-16 01:19:16 +0530

    bug fix 1

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const User = require('../models/user')
+const User = require('../models/User')
 const { JWT_SECRET } = require("../config");
 const jwt = require('jsonwebtoken');
 const db = require('../db')
